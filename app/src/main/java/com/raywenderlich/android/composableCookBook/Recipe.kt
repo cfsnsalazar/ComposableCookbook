472ad490fa0549ad872655a71c9ee3d9ea046c28
Napoleon Salazar <nsalazar@coolfiresolutions.com>
2020-10-10 23:52:28 -0500

    First Commit

---
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/Recipe.kt
@@ -0,0 +1,7 @@
+package com.raywenderlich.android.composableCookBook
+
+import androidx.annotation.DrawableRes
+
+data class Recipe(@DrawableRes val imageResource: Int,
+                  val title: String,
+                  val ingredients: List<String>)
